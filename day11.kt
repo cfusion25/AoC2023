7ca107b57ed9aa12ebe36ca23d2dbb2746509708
cfusion25 <152647782+cfusion25@users.noreply.github.com>
2023-12-11 12:13:07 -0500

    moved solution to a function
    now prints answers to both part

--- day11.kt
+++ day11.kt
@@ -2,18 +2,7 @@ import java.io.File
 import java.io.InputStream
 import kotlin.math.abs
 
-fun main(args: Array<String>) {
-    val inputStream: InputStream = File("C:\\Users\\coldf\\OneDrive\\Desktop\\AoC2023\\input\\d11.txt").inputStream()
-    val lineList = mutableListOf<MutableList<Char>>()
-
-    inputStream.bufferedReader().forEachLine { line->
-        val charList = mutableListOf<Char>()
-        line.forEach {
-            charList.add(it)
-        }
-        lineList.add(charList)
-    }
-
+fun getDistance(eSize: Int, lineList: MutableList<MutableList<Char>>) {
     val initialGalaxyList = mutableListOf<Pair<Int,Int>>()
 
     lineList.forEachIndexed() { i, line->
@@ -27,53 +16,21 @@ fun main(args: Array<String>) {
     val yExpand = mutableListOf<Int>()
     val xExpand = mutableListOf<Int>()
 
-    // For part 1 set to 1, for part 2 set to 999999
-    val eSize = 999999
-
     for(i in 0..< lineList.size) {
         var galaxy = false
         for (j in 0..<lineList[0].size) {
-            if (lineList[i][j] == '#') {
-                galaxy = true
-            }
+            if (lineList[i][j] == '#') { galaxy = true }
         }
-        if(!galaxy) {
-            yExpand.add(i)
-        }
+        if(!galaxy) { yExpand.add(i)}
     }
 
     for(j in 0..<lineList[0].size) {
         var galaxy = false
         for (i in 0..<lineList.size) {
-            if (lineList[i][j] == '#') {
-                galaxy = true
-            }
+            if (lineList[i][j] == '#') { galaxy = true }
         }
-        if(!galaxy) {
-            xExpand.add(j)
-        }
+        if(!galaxy) { xExpand.add(j) }
     }
-
-    // Prints the map for debugging
-//    lineList.forEach {
-//        it.forEach {c ->
-//            print(c)
-//        }
-//        println()
-//    }
-
-    val galaxyList = mutableListOf<Pair<Int,Int>>()
-
-    lineList.forEachIndexed() { i, line->
-        line.forEachIndexed() { j, c->
-            if(c == '#') {
-                galaxyList.add(Pair(i,j))
-            }
-        }
-    }
-
-    println(xExpand)
-    println(yExpand)
 
     var dSum:Long = 0
     for (i in 0..<initialGalaxyList.size) {
@@ -88,24 +45,32 @@ fun main(args: Array<String>) {
             var yit2 = 0
 
             yExpand.forEach {
-                if(g1.first > it) {
-                    yit1++
-                }
-                if(g2.first > it) {
-                    yit2++
-                }
+                if(g1.first > it) { yit1++ }
+                if(g2.first > it) { yit2++ }
             }
             xExpand.forEach {
-                if(g1.second > it) {
-                    xit1++
-                }
-                if(g2.second > it) {
-                    xit2++
-                }
+                if(g1.second > it) { xit1++ }
+                if(g2.second > it) { xit2++ }
             }
 
             dSum += abs((g1.first + (yit1 * eSize)) - (g2.first + (yit2 * eSize))) + abs((g1.second + (xit1 * eSize)) - (g2.second + (xit2 * eSize)))
         }
     }
     println(dSum)
+}
+
+fun main(args: Array<String>) {
+    val inputStream: InputStream = File("C:\\Users\\coldf\\OneDrive\\Desktop\\AoC2023\\input\\d11.txt").inputStream()
+    val lineList = mutableListOf<MutableList<Char>>()
+
+    inputStream.bufferedReader().forEachLine { line->
+        val charList = mutableListOf<Char>()
+        line.forEach {
+            charList.add(it)
+        }
+        lineList.add(charList)
+    }
+
+    getDistance(1,lineList)
+    getDistance(999999,lineList)
 }
